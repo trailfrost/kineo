2f2db9b6ac89e93896a31f5cc0f51c883a43fcb6
trailfrost <winter.trailfrost@gmail.com>
2025-07-28 14:22:32 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Kineo
+
+Object-Graph Mapper for Neo4j in TypeScript.
+
+## Contributing
+
+Contributions are welcome! Just make sure to dicuss changes you want to make in [Discussions](https://github.com/trailfrost/kineo/discussions), and discuss bug reports in [Issues](https://github.com/trailfrost/kineo/issues).
+
+## License
+
+This project uses the [MIT License](LICENSE).
---
+++ src/schema.ts
@@ -0,0 +1,146 @@
+export type CypherType =
+  | "ANY"
+  | "BOOLEAN"
+  | "DATE"
+  | "DURATION"
+  | "FLOAT"
+  | "INTEGER"
+  | "LOCAL DATETIME"
+  | "LOCAL TIME"
+  | "MAP"
+  | "NODE"
+  | "NOTHING"
+  | "PATH"
+  | "POINT"
+  | "PROPERTY VALUE"
+  | "RELATIONSHIP"
+  | "STRING"
+  | "ZONED DATETIME"
+  | "ZONED TIME";
+
+export type Direction = "IN" | "OUT" | "BOTH";
+
+export type Node = Record<string, FieldDef | RelationshipDef>;
+export type Schema = Record<string, Node>;
+
+export class FieldDef {
+  cypherTypes: CypherType[];
+  defaultValue: unknown | null;
+  isPrimaryKey: boolean = false;
+  isRequired: boolean = false;
+  isUnique: boolean = false;
+  isArray: boolean = false;
+
+  constructor(...types: CypherType[]) {
+    this.cypherTypes = types;
+  }
+
+  id() {
+    this.isPrimaryKey = true;
+    this.isRequired = true;
+    return this;
+  }
+
+  types(...types: CypherType[]) {
+    this.cypherTypes.concat(types);
+    return this;
+  }
+
+  default(value: unknown) {
+    this.defaultValue = value;
+    this.isRequired = true;
+    return this;
+  }
+
+  required() {
+    this.isRequired = true;
+    return this;
+  }
+
+  optional() {
+    this.isRequired = false;
+    return this;
+  }
+
+  array() {
+    this.isArray = true;
+    return this;
+  }
+}
+
+export class RelationshipDef {
+  refTo: string;
+  refLabel: string = "UNNAMED";
+  refDirection: Direction = "OUT";
+  isRequired: boolean = false;
+  isArray: boolean = false;
+  defaultValue: unknown | null = null;
+
+  constructor(to: string) {
+    this.refTo = to;
+  }
+
+  to(refTo: string) {
+    this.refTo = refTo;
+    return this;
+  }
+
+  label(name: string) {
+    this.refLabel = name;
+    return this;
+  }
+
+  outgoing(label: string) {
+    this.refDirection = "OUT";
+    this.refLabel = label;
+    return this;
+  }
+
+  incoming(label: string) {
+    this.refDirection = "IN";
+    this.refLabel = label;
+    return this;
+  }
+
+  both(label: string) {
+    this.refDirection = "OUT";
+    this.refLabel = label;
+    return this;
+  }
+
+  direction(dir: Direction) {
+    this.refDirection = dir;
+    return this;
+  }
+
+  required() {
+    this.isRequired = true;
+    return this;
+  }
+
+  default(value: unknown) {
+    this.defaultValue = value;
+    return this;
+  }
+
+  array() {
+    this.isArray = true;
+    return this;
+  }
+}
+
+export function field(...types: CypherType[]): FieldDef {
+  return new FieldDef(...types);
+}
+
+export function relation(to: string): RelationshipDef {
+  return new RelationshipDef(to);
+}
+
+export function node<TNode extends Node>(def: TNode): TNode {
+  return def;
+}
+
+export function defineSchema<TSchema extends Schema>(def: TSchema): TSchema {
+  return def;
+}

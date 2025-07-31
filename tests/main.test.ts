a3af35e3e6902371b27e6ebb6e8543cbf73fa985
trailfrost <winter.trailfrost@gmail.com>
2025-07-31 20:22:24 -0300

    ci: add ci

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -2,15 +2,6 @@ import { describe, expect, test } from "vitest";
 import { db } from "./utils";
 
 describe("Connecting to the database", async () => {
-  test("Server information", async () => {
-    const info = await db.driver.getServerInfo();
-    expect(info).toMatchObject({
-      address: "localhost:7687",
-      agent: "Neo4j/2025.06.2",
-      protocolVersion: 5.8,
-    });
-  });
-
   test("Running Cypher query", async () => {
     const { records } = await db.cypher(`
       MATCH (n)

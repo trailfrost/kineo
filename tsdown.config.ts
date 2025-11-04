2e00cb0382e4229c3c6419ce66e21f1206e03033
trailfrost <winter.trailfrost@gmail.com>
2025-11-04 19:06:39 -0300

    fix: cli didn't run through command, add tests for cli

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,10 +2,16 @@ import { defineConfig } from "tsdown";
 import pkg from "./package.json" with { type: "json" };
 
 export default defineConfig({
-  entry: Object.entries(pkg.exports).map(([, value]) =>
-    value.import.replace("./dist", "./src").replace(".mjs", ".ts")
-  ),
+  entry: [
+    ...Object.entries(pkg.bin).map(([, value]) => format(value)),
+    ...Object.entries(pkg.exports).map(([, value]) => format(value.import)),
+  ],
   tsconfig: "./tsconfig.app.json",
   format: ["esm"],
   dts: true,
+  minify: true,
 });
+
+function format(str: string) {
+  return str.replace("./dist", "./src").replace(".mjs", ".ts");
+}

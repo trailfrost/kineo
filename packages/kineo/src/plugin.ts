4c94142437b3115e74f88b54673ad7ffc92145b5
trailfrost <winter.trailfrost@gmail.com>
2025-12-08 16:16:22 -0300

    feat: add pull and generate commands to neo4j adapter

--- packages/kineo/src/plugin.ts
+++ packages/kineo/src/plugin.ts
@@ -2,8 +2,20 @@ import type { OptPromise } from "./adapter";
 import type { Kineo } from "./client";
 import type { Model } from "./model";
 
+/**
+ * A Kineo plugin.
+ */
 export interface Plugin {
+  /**
+   * Runs when a plugin is being initialized.
+   * @param client The initialized client.
+   */
   init?(client: Kineo<any, any>): OptPromise<void>;
 
+  /**
+   * Runs when an operation is run on a model.
+   * @param model The model the operation was executed on.
+   * @param op The operation.
+   */
   onExec?(model: Model<any, any>, op: string): OptPromise<void>;
 }
